477b0dbfa9fae09baff0c8b475598d98f76d5906
Yusupov Elbek <elbekyusupovclever@gmail.com>
2024-08-08 14:49:16 +0500

    asdas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import '123123./index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.123createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
