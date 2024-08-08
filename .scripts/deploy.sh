1f34937d8ed33d2429951dfb70f840903369c3dc
Yusupov Elbek <elbekyusupovclever@gmail.com>
2024-08-08 11:51:42 +0500

    asdasd

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 set -e
 
+# nvm muhitini sozlash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 echo "Deployment started..."
 
+# Pull the latest version of the app
+cd /home/salom
+git stash
+git pull
+echo "New changes copied to server!"
 
+# Install dependencies and build
 echo "Installing Dependencies..."
+npm install --yes
 
-          export NVM_DIR="$HOME/.nvm"
-          [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-          [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-          whoami
-          git stash
-          git pull
-          npm install --yes
-          npm run build
+echo "Creating Production Build..."
+npm run build
 
 echo "Deployment Finished!"
